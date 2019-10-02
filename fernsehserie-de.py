12ba2bee042ce60dbe60fc8c295b7e7057c9bb16
StefanAbl <48151667+StefanAbl@users.noreply.github.com>
2019-10-02 14:29:41 +0200

    implemented levenshtein

--- fernsehserie-de.py
+++ fernsehserie-de.py
@@ -30,9 +30,7 @@ def main(argv):
             episode_name = arg
         elif opt in ("-d"):
             debug = True
-    
-    import re
-    regex = re.compile(".*?\((.*?)\)")        
+           
     if debug:
         print('Show name is ' + show_name)
         print('episode name is ' + episode_name)
@@ -45,24 +43,27 @@ def main(argv):
         print("URL is: " + url)
 
     #get the search page
-    #data = urllib.parse.urlencode(values)
-    #data = data.encode('utf-8')
     req = urllib.request.Request(url) 
-    resp = urllib.request.urlopen(req) 
-    respData = resp.read() 
-    soup = BeautifulSoup(respData, features="html.parser")
-
-    #parse the search page to get the link to the series
-    link = soup.find("main")
-    link = link.find("ul")
-    link = link.find("li")
-    link = link.find("a")
-    link = link['href']
-
-    #construct episode url
-    url = 'https://www.fernsehserien.de' + link + '/episodenguide'
+    resp = urllib.request.urlopen(req)
     if debug:
-        print("URL to show page is: " + url)
+        print("Response URL is: " + str(resp.geturl()))
+    if "suche" in str(resp.geturl()):
+        respData = resp.read() 
+        soup = BeautifulSoup(respData, features="html.parser")
+
+        #parse the search page to get the link to the series
+        link = soup.find("main")
+        link = link.find("ul")
+        link = link.find("li")
+        link = link.find("a")
+        link = link['href']
+
+        #construct episode url
+        url = 'https://www.fernsehserien.de' + link + '/episodenguide'
+        if debug:
+            print("URL to show page is: " + url)
+    else:
+        url = str(resp.geturl()) + '/episodenguide'
 
     #get show page
     req = urllib.request.Request(url) 
@@ -95,41 +96,47 @@ def main(argv):
     if debug:
         printMatrix(Matrix)
     #find the episode
-    location = -1
-    for i in range(len(Matrix)):
-        inrow = False
-        for cell in Matrix[i]:
-            if not cell.find(episode_name) == -1:
-                inrow = True
-                location = i
-        if inrow:
-            if debug:
-                stringy = ""
-                for cell in Matrix[location]:
-                    stringy = stringy + cell + "|"
-                print("Found in this line: " + stringy)
-            break
-    if location == -1:
-        print("An episode by this name could not be found mybe there was an error try with `-d`")
-    else:
-        season_number = str(Matrix[location][3])[:-1]
-        if len(season_number) == 1:
-            season_number = "0" + season_number
-        number = "S" + season_number + "E" + Matrix[location][4]
-        print("The episodes number is: " + number)
+    #replaced by using the levensthein algorithmen to determine the closest match and not just 
+    #looking for exact matches
+    # location = -1
+    # for i in range(len(Matrix)):
+    #     inrow = False
+    #     for cell in Matrix[i]:
+    #         if not cell.find(episode_name) == -1:
+    #             inrow = True
+    #             location = i
+    #     if inrow:
+    #         if debug:
+    #             stringy = ""
+    #             for cell in Matrix[location]:
+    #                 stringy = stringy + cell + "|"
+    #             print("Found in this line: " + stringy)
+    #         break
+    # if location == -1:
+    #     print("An episode by this name could not be found mybe there was an error try with `-d`")
+    # else:
+    #     season_number = str(Matrix[location][3])[:-1]
+    #     if len(season_number) == 1:
+    #         season_number = "0" + season_number
+    #     number = "S" + season_number + "E" + Matrix[location][4]
+    #     print("The episodes number is: " + number)
         
 
     #find the location using the results of the levenshtein algorithm
-    for row in Matrix:
-        try:
-            Matrix[i][len(row) - 1] = int(Matrix[i][len(row) - 1])
-        except ValueError:
-            print("Could not convert " + Matrix[i][len(row) - 1] + " to int")
     df = pd.DataFrame(Matrix)
-    print(len(df.columns))
-    print(df)
-    print(df.sort_values('10' , na_position='last'))
-    number = number
+    df=df.drop(0,axis=1)
+    df=df.drop(2,axis=1)
+    df=df.drop(5,axis=1)
+    df[10] = df[10].astype(int)
+    df = df.sort_values(by=10 , na_position='last')
+    if debug:
+        print(df)
+    season_number = str(df[3].iloc[0])[:-1]
+    if len(season_number) == 1:
+        season_number = "0" + season_number
+    number = "S" + season_number + "E" + str(df[4].iloc[0])
+    print("The episodes number is: " + number)
+
     return number
 
 def levenshtein(seq1, seq2):
